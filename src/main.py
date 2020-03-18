8be18c7112b0de16e91a2417dd0dd159dd4fdc7a
Viljami Raiski <viljami.raiski@aalto.fi>
2020-03-18 14:09:39 +0200

    Sliding window added

---
+++ src/main.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import numpy as np
+import hrvanalysis as hrv
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+from hrvanalysis import plot_poincare
+from hrvanalysis import remove_outliers, remove_ectopic_beats, interpolate_nan_values
+from hrvanalysis import plot_psd
+
+from utils import return_sliding_window, plot_time_series
+import utils as utils
+
+
+def save_to_csv(df, filename):
+    df.to_csv(filename,
+              index=False, header=True)
+
+
+def pretty(d, indent=0):
+    i = 0
+    for key, value in d.items():
+        print('\t' * indent + str(key))
+        if isinstance(value, dict):
+            pretty(value, indent+1)
+        else:
+            print('\t' * (indent+1) + str(value))
+
+
+def main():
+    print("Importing dataset")
+    sleep_data = pd.read_csv("./sleep_16_2.csv")
+    # sleep_data_full = pd.read_csv("./sleep_full.csv")
+
+    # sleep_data_full.time = pd.to_datetime(sleep_data_full.time, unit='s')
+    print("---------------------")
+    print("Resulting dataset")
+    print(sleep_data.head())
+    sleep_data.time = pd.to_datetime(sleep_data.time, unit='s')
+
+    # sleep_not_null = sleep_data[sleep_data.hrv > 0]
+    # rr_data = sleep_data[sleep_data.bbt0 > 0]
+    # rr_data = rr_data[rr_data.bbt0 < 1500]
+    # print(sleep_not_null)
+    # save_to_csv(rr_data.bbt0, "clean_hrv.csv")
+
+    sleep_data.bbt0 = sleep_data.bbt0.replace(0, 750)
+    print(sleep_data.head())
+    sleep_data.loc[(sleep_data.bbt0 > 1500), 'bbt0'] = 1500
+    # print(sleep_data.head())
+    # print(sleep_data.bbt0.mean())
+
+    nn_intervals_list = list(sleep_data.bbt0)
+
+    sns.set()
+
+    # Plotting heart rate
+    # sns.lineplot(x='time', y='hr', data=sleep_data,
+    #              marker="o", linewidth=0,  alpha=0.9, ms=3, mew=0.1).set(title="HR")
+    # plt.show()
+
+    # Plotting heart rate and Heart rate variability
+    fig, ax = plt.subplots()
+    sns.lineplot(x='time', y='hr', data=sleep_data, alpha=0.8, marker='o', linewidth=0, ms=2, mew=0.1, color="orange").set(
+        title="HRV and HR")
+    ax.set_ylabel("heart rate")
+
+    ax2 = ax.twinx()
+    sns.lineplot(x='time', y='hrv', data=sleep_data,
+                 linewidth=0, marker="o", ms=2, mew=0.1)
+    ax2.set_ylabel("heart rate variability")
+    plt.show()
+
+    # # This remove outliers from signal
+    # rr_intervals_without_outliers = remove_outliers(rr_intervals=nn_intervals_list,
+    #                                                 low_rri=500, high_rri=1500)
+    # # # This replace outliers nan values with linear interpolation
+    # interpolated_rr_intervals = interpolate_nan_values(rr_intervals=rr_intervals_without_outliers,
+    #                                                    interpolation_method="linear")
+
+    # # This remove ectopic beats from signal
+
+    # nn_intervals_list = np.array(remove_ectopic_beats(
+    #     rr_intervals=interpolated_rr_intervals, method="malik"))
+    # print(~np.isnan(nn_intervals_list))
+    # rr_intervals_without_outliers = np.array(rr_intervals_without_outliers)
+    # rr_intervals_without_outliers = rr_intervals_without_outliers[~np.isnan(
+    #     rr_intervals_without_outliers)]
+    # nn_intervals_list = np.array(remove_ectopic_beats(
+    #     rr_intervals=rr_intervals_without_outliers, method="malik"))
+
+    # # time_domain_features = hrv.get_time_domain_features(nn_intervals_list)
+
+    # # Time domain features
+    time_domain_features = utils.return_time_domain_features(nn_intervals_list)
+    print("Time-domain features")
+    print(time_domain_features)
+    pretty(time_domain_features)
+
+    # # Poincare-plot features
+    poincare_plot_features = utils.return_poincare_plot_features(
+        nn_intervals_list)
+    print("Poincare-plot features")
+    print(poincare_plot_features)
+    pretty(poincare_plot_features)
+
+    # # Geometrical features
+    geometrical_features = utils.return_poincare_plot_features(
+        nn_intervals_list)
+    print("Geometrical features")
+    print(geometrical_features)
+    pretty(geometrical_features)
+
+    # # Frequency-domain  features
+    frequency_domain_features = utils.return_frequency_domain_features(
+        nn_intervals_list)
+    print("Frequency-domain features")
+    print(frequency_domain_features)
+    pretty(frequency_domain_features)
+
+    # hrv.plot_timeseries(rr_intervals_without_outliers)
+    # plot_poincare(rr_intervals_without_outliers, plot_sd_features=True)
+
+    # plot_psd(rr_intervals_without_outliers, method="welch")
+    # plot_psd(nn_intervals_list, method="lomb")
+    # plot_psd(rr_intervals_without_outliers, method="lomb")
+
+    w_size = 300
+
+    lf_hf_results = []
+    timestamps = []
+
+    # Get timestamps for sliding window
+    timestamps = utils.return_sliding_window_time(
+        sleep_data.time, nn_intervals_list, w_size)
+
+    # Get LF/HF-ratio for sliding window
+    lf_hf_results = utils.return_sliding_window_data(
+        nn_intervals_list, w_size, utils.return_frequency_domain_features, 'lf_hf_ratio')
+
+    # # Plotting beat to beat-data
+    # plot_time_series(sleep_data.time, sleep_data.bbt0)
+
+    # # Plotting LF/HF-data
+    sns.set()
+    title = "LF/HF-ratio (window size: {}) ".format(w_size)
+    plot_time_series(timestamps, lf_hf_results, title=title)
+
+
+main()
---
+++ src/utils.py
@@ -0,0 +1,71 @@
+from itertools import islice
+import seaborn as sns
+import matplotlib.pyplot as plt
+import hrvanalysis as hrv
+
+
+def plot_time_series(T, Y, title=" ", *args):
+    sns.lineplot(x=T, y=Y).set(title=title)
+    plt.show()
+
+
+def save_df_to_csv(df, filename):
+    df.to_csv(filename,
+              index=False, header=True)
+
+
+def return_sliding_window(input_list, n, step_size):
+    iterable = iter(input_list)
+    result = list(islice(iterable, n, None, step_size))
+    if len(result) == n:
+        yield result
+    for elem in iterable:
+        result = result[1:] + list((elem,))
+        yield result
+
+
+def return_time_domain_features(rr_i):
+    time_domain_features = hrv.get_time_domain_features(
+        rr_i)
+    return time_domain_features
+
+
+def return_poincare_plot_features(rr_i):
+    poincare_plot_features = hrv.get_poincare_plot_features(
+        rr_i)
+    return poincare_plot_features
+
+
+def return_geometrical_features(rr_i):
+    geometrical_features = hrv.get_geometrical_features(
+        rr_i)
+    return geometrical_features
+
+
+def return_frequency_domain_features(rr_i):
+    frequency_domain_features = hrv.get_frequency_domain_features(
+        rr_i)
+    return frequency_domain_features
+
+
+def return_single_feature(feature_dict, feature_name):
+    return feature_dict[feature_name]
+
+
+def return_sliding_window_data(rr_i, w_size, feature_fun, feature_name):
+    results = []
+    for i in range(len(rr_i)-w_size-1):
+        data_window = rr_i[i:i+w_size-1]
+        features = feature_fun(data_window)
+        results.append(return_single_feature(features, feature_name))
+    return results
+
+
+def return_sliding_window_time(time_data, rr_i,  w_size):
+    # Args:
+    # time_data: pd.Dataframe
+    # w_size: int
+    timestamps = []
+    for i in range(len(rr_i)-w_size-1):
+        timestamps.append(time_data.iloc[int(i + w_size/2)])
+    return timestamps
